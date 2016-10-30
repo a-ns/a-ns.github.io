34ee6a001fe63692defddac5c6c14b7b2153ccb5
AleksanderS <exmann94@gmail.com>
2016-10-30 12:50:43 -0500

    Updated bundle.js

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
 export default class Footer extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      links: []
+    }
+    this.state.links.push({'link': 'http://github.com/a-ns/a-ns.github.io', 'text': 'Source Code'})
+    this.state.links.push({'link': 'http://github.com/a-ns', 'text': 'Github'})
+    this.state.links.push({'link': 'https://facebook.github.io/react/', 'text': 'React'})
+    this.state.links.push({'link': 'http://MaterializeCSS.com', 'text': 'MaterializeCSS'})
+  }
+
+  formatLink (link) {
+    return <li><a target='_blank' className='grey-text text-lighten-3' href={link.link}> {link.text} </a></li>
+  }
   render () {
     return (
-      // <footer className='z-depth-2 page-footer blue darken-1 container'>
-      //   <div className='row valign-wrapper center'>
-      //     <div className='col s1 m4 l2'><p>Lorem</p></div>
-      //     <div className='col s1 m4 l8'><p>Ipsum</p></div>
-      //     <div className='col s1 m4 l2'><a target='_blank' className='black-text text-lighten-3' href='http://www.github.com/a-ns'><img style={{height: '25.5px', width: '25.5px'}} src='https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png' alt='' className='circle responsive-img'></img></a>
-      //   </div>
-      //   </div>
-      // </footer>
       <footer className='page-footer blue darken-1'>
        <div className='container'>
          <div className='row'>
@@ -21,11 +27,7 @@ export default class Footer extends React.Component {
            <div className='col l4 offset-l2 s12'>
              <h5 className='white-text'>Links</h5>
              <ul>
-               <li><a target='_blank' className='grey-text text-lighten-3' href='http://github.com/a-ns/a-ns.github.io'>Source Code</a></li>
-               <li><a target='_blank' className='grey-text text-lighten-3' href='http://github.com/a-ns'>Github</a></li>
-               <li><a target='_blank' className='grey-text text-lighten-3' href='https://facebook.github.io/react/'>React</a></li>
-               <li><a target='_blank' className='grey-text text-lighten-3' href='http://MaterializeCSS.com'>MaterializeCSS</a></li>
-               <li><a target='_blank' className='grey-text text-lighten-3' href='#!'></a></li>
+               {this.state.links.map(this.formatLink)}
              </ul>
            </div>
          </div>
