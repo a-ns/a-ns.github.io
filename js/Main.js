cffab6996bfe1e0c73c8cd91806aacc93f16c71e
Alex Lindemann <exmann94@gmail.com>
2016-11-22 14:00:20 -0600

    re add Main.js

---
+++ js/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Router from './Router'
+
+class App extends React.Component {
+  render () {
+    return (
+      <Router />
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
